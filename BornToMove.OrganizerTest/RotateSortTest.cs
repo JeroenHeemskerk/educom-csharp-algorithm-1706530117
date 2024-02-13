0285883d6f4274bd079502f6279056b81da373ca
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2024-02-13 15:41:14 +0100

    first start of Unit Test Class

---
+++ BornToMove.OrganizerTest/RotateSortTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace BornToMove.OrganizerTest
+{
+    public class RotateSortTest 
+    {
+        [Test]
+
+        public void testSortEmpty()
+        {
+
+        }
+
+        public void testSortOneElement()
+        {
+
+        }
+
+        public void testSortTwoElements()
+        {
+            // prepare
+            RotateSort<int> sorter = new RotateSort<int>();
+            List<int> input = new List<int>() { 32, 3 };
+            IComparer<int> = Comparer<int>.Default;
+
+            // run
+            var result = sorter.Sort(input, comparer);
+
+            // validate
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Has.Exactly(2).Items);
+            Assert.That(result, Is.EquivalentTo(new int[] { 3, 32 }));
+            // also check that our input is not modified
+            Assert.That(input, Is.EquivalentTo(new int[] { 32, 3 }));
+        }
+
+        public void testSortThreeEqual()
+        {
+
+        }
+
+        public void testSortUnsortedArray()
+        {
+
+        }
+
+    }
+}
